5f6d99aa5ae11df4f8a6943bb73fb81e144fa3dd
nif0 <nif0@yandex.ru>
2021-03-19 12:30:33 +0500

    доработка
    вывод списка файлов в столбик. Есть возможность в исходниках выставлять флаги для управления выводом информации на экран.

--- java/mini_utils/ls/src/com/company/Main.java
+++ java/mini_utils/ls/src/com/company/Main.java
@@ -9,10 +9,10 @@ package com.company;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedList;
+import java.text.DateFormat;
+import java.util.*;
 import java.util.regex.*;
+import java.nio.file.Files;
 
  class DirFilter implements  FilenameFilter{
     private Pattern pattern;
@@ -28,105 +28,123 @@ import java.util.regex.*;
 }
 
 public class Main {
+    private static enum FilePropertyNames {
+        PARENT,      //parent folder
+        ABSPATH,     // absolute path
+        FILENAME,
+        CANPATH,     // canonical path
+        FREESPACE,
+        TOTALSPACE,
+        USABLESPACE,
+        HASHCODE,
+        EXECUTE,
+        //атрибуты файла
+        WRITEF,     // доступ на запись
+        READF,      // доступ на чтение
+        HIDDENF,    // скрытый
+        LMTIME,      // last modify time
+        OWNER
+    }
+
+    private static Map<FilePropertyNames,Boolean> printProperty = new HashMap<>();
 
     static ArrayList<File> fileAList = new ArrayList<File>();
 
     private static void printFileProperty(File f) {
-        System.out.print("absolute path: ");
+        StringBuilder result = new StringBuilder();
         try {
-            System.out.println(f.getAbsolutePath());
+            if (printProperty.get(FilePropertyNames.ABSPATH)) {
+                result.append(" " + f.getAbsolutePath());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("parent: ");
         try {
-            System.out.println(f.getParent());
+            if (printProperty.get(FilePropertyNames.PARENT)) {
+                result.append(" " + f.getParent());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("file name: ");
         try {
-            System.out.println(f.getName());
+            if (printProperty.get(FilePropertyNames.FILENAME)) {
+                result.append(" " + f.getName());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("canonical path: ");
         try {
-            System.out.println(f.getCanonicalPath());
+            if (printProperty.get(FilePropertyNames.CANPATH)) {
+                result.append(" " + f.getCanonicalPath());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("free space: ");
         try {
-            System.out.println(f.getFreeSpace());
+            if (printProperty.get(FilePropertyNames.FREESPACE)) {
+                result.append(" " + f.getFreeSpace());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("total space: ");
         try {
-            System.out.println(f.getTotalSpace());
+            if (printProperty.get(FilePropertyNames.TOTALSPACE)) {
+                result.append(" " + f.getTotalSpace());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("usable space: ");
         try {
-            System.out.println(f.getUsableSpace());
+            if (printProperty.get(FilePropertyNames.USABLESPACE)) {
+                result.append(" " + f.getUsableSpace());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("hash code: ");
         try {
-            System.out.println(f.hashCode());
+            if (printProperty.get(FilePropertyNames.HASHCODE)) {
+                result.append(" " + f.hashCode());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.print("execute: ");
         try {
-            System.out.println(f.canExecute());
+            if (printProperty.get(FilePropertyNames.EXECUTE)) {
+                result.append(" " + f.canExecute());
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println();
         }
-
-        System.out.print("write: ");
         try {
-            System.out.println(f.canWrite());
+            if (printProperty.get(FilePropertyNames.WRITEF)) {
+                result.append(" " + f.canWrite());
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println();
         }
-
-        System.out.print("read: ");
         try {
-            System.out.println(f.canRead());
+            if (printProperty.get(FilePropertyNames.READF)) {
+                result.append(" " + f.canRead());
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println();
         }
-
-        System.out.print("hiden flag: ");
         try {
-            System.out.println(f.isHidden());
+            if (printProperty.get(FilePropertyNames.HIDDENF)) {
+                result.append((" " + f.isHidden()) );
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println();
         }
-
-        System.out.print("last modify time: ");
         try {
-            System.out.println(new Date(f.lastModified()).toLocaleString());
+            if (printProperty.get(FilePropertyNames.LMTIME)) {
+                result.append(" " + new Date(f.lastModified()).toLocaleString().replaceAll(" ",""));
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println();
         }
+        //DateFormat.
+        System.out.println(result.substring(0));
     }
 
     private static void addAllFilesInList(File file) {
@@ -145,27 +163,28 @@ public class Main {
 
 
     public static void main(String[] args)  {
-
-      //  args[0] = "F:\\projects\\java\\mini_utils\\ls";
+        printProperty.put(FilePropertyNames.ABSPATH,false);
+        printProperty.put(FilePropertyNames.FILENAME,true);
+        printProperty.put(FilePropertyNames.CANPATH,false);
+        printProperty.put(FilePropertyNames.EXECUTE,false);
+        printProperty.put(FilePropertyNames.FREESPACE,false);
+        printProperty.put(FilePropertyNames.HASHCODE,false);
+        printProperty.put(FilePropertyNames.HIDDENF,false);
+        printProperty.put(FilePropertyNames.LMTIME,true);
+        printProperty.put(FilePropertyNames.OWNER,false);
+        printProperty.put(FilePropertyNames.PARENT,false);
+        printProperty.put(FilePropertyNames.READF,false);
+        printProperty.put(FilePropertyNames.TOTALSPACE,false);
+        printProperty.put(FilePropertyNames.USABLESPACE,false);
+        printProperty.put(FilePropertyNames.WRITEF,false);
 	//условие досрочного завершения.
         if (args == null || args.length > 1 || args.length == 0) {
             return;
         }
-      //  Path path = Path.of(args[0]);
-    //    path = path.normalize();
-      //  System.out.println(path);
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
         String param = args[0];
         File file = new File(param);
-        try {
-            System.out.println(file.getAbsolutePath());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
         LinkedList listFiles;
-        //FFilter filter = new FFilter(param);
         /*
         I separate the existing part of the path from the file name mask
         and eventually expect a file object with the existing path (part of the param string).
@@ -174,9 +193,6 @@ public class Main {
 
         while (!file.exists()) {
             file = new File(file.getParent());
-            System.out.println("tick");
-            System.out.println("new work path: " + file.getPath());
-         //   System.out.println(param.compareTo(file.getPath()));
         }
         /*
              it is quite strange to use the compare method to determine the position of the mask
@@ -188,17 +204,10 @@ public class Main {
         */
         int delta = param.compareTo(file.getPath());
         String mask = param.substring(param.length()-delta+1,param.length());
-        System.out.println("work path: " + file.getPath());
-        System.out.println("mask: " + mask);
         addAllFilesInList(file);
-
-        //DirFilter dirFilter = new DirFilter(".*xml$");
-        //listFiles. //= file;
-        //listFiles.
 
         for (File f: fileAList
              ) {
-            System.out.println();
             printFileProperty(f);
         }
     }
