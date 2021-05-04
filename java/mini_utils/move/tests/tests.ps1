5d2c48d85eda2cf1991edcf6e17e0bb6a6859a54
nif0 <nif0@yandex.ru>
2021-05-04 08:56:38 +0500

    deleted:    java/mini_utils/move/tests/f1/text docum4.txt 	deleted:    java/mini_utils/move/tests/f1/text documen1.txt 	deleted:    java/mini_utils/move/tests/f1/text document.txt 	deleted:    java/mini_utils/move/tests/f1/text document2.txt 	deleted:    java/mini_utils/move/tests/f1/text document3.txt 	deleted:    java/mini_utils/move/tests/f1/text document5.txt 	modified:   java/mini_utils/move/tests/tests.ps1

--- java/mini_utils/move/tests/tests.ps1
+++ java/mini_utils/move/tests/tests.ps1
@@ -1,8 +1,35 @@
-﻿Cloning into 'my_repo'...
-remote: Enumerating objects: 594, done.
-remote: Counting objects: 100% (594/594), done.
-remote: Compressing objects: 100% (252/252), done.
-remote: Total 594 (delta 141), reused 567 (delta 114), pack-reused 0
-Receiving objects: 100% (594/594), 273.51 KiB | 345.00 KiB/s, done.
-Resolving deltas: 100% (141/141), done.
-Updating files: 100% (42/42), done.
+﻿#тесты с каталогами
+#работают без ошибок
+#переименование каталога
+#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2 F:\git\my_repo\java\mini_utils\move\tests\f2_
+#sleep 10;
+#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2_ F:\git\my_repo\java\mini_utils\move\tests\f2
+sleep 10;
+echo "start test2"
+
+#перемещение 
+#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2 F:\git\my_repo\java\mini_utils\move\tests\f1\
+#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f1\f2 F:\git\my_repo\java\mini_utils\move\tests\f2
+#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f1\f2 F:\git\my_repo\java\mini_utils\move\tests\f2
+#..\move\out\artifacts\move_jar\move.jar ..\tests\f2 ..\tests\f1\f2
+#..\move\out\artifacts\move_jar\move.jar ..\tests\f1\f2 ..\tests\f2
+
+#одиночные файлы
+#работают с ошибками.
+#после перемещения вв начале имени добавляется знак ; Востальном перемещения работают.
+#test1_result.txt -> ;test1_result.txt
+
+
+#;test1_result.txt ->
+##..\move\out\artifacts\move_jar\move.jar "..\tests\;test1_result.txt" "F:\git\my_repo\java\mini_utils\move\tests\"
+## -> ;;test1_result.txt
+
+#
+#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\test1_result.txt ..\tests\f2\
+#..\move\out\artifacts\move_jar\move.jar "..\tests\f2\;;;;test1_result.txt" "F:\git\my_repo\java\mini_utils\move\tests\"
+
+
+
+#группы файлов. Имя задаётся регуляркой.
+#полностью не работает
+..\move\out\artifacts\move_jar\move.jar "F:\git\my_repo\java\mini_utils\move\tests\^.*.txt" F:\git\my_repo\java\mini_utils\move\tests\f1\
