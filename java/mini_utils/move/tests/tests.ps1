6344b1f575b75ab761fedf851c037ac04ad16188
nif0 <nif0@yandex.ru>
2021-05-15 10:19:10 +0500

    update move tests
    все тесты работают

--- java/mini_utils/move/tests/tests.ps1
+++ java/mini_utils/move/tests/tests.ps1
@@ -1,38 +1,60 @@
 ﻿#тесты с каталогами
 #работают без ошибок
 #переименование каталога
-#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2 F:\git\my_repo\java\mini_utils\move\tests\f2_
-#sleep 10;
-#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2_ F:\git\my_repo\java\mini_utils\move\tests\f2
+echo "start test1"
+echo 1;
+..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2_ F:\git\my_repo\java\mini_utils\move\tests\f2
+sleep 10;
+echo 2;
+..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2 F:\git\my_repo\java\mini_utils\move\tests\f2_
+sleep 10;
+echo 3;
+..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2_ F:\git\my_repo\java\mini_utils\move\tests\f2
 sleep 10;
 echo "start test2"
 
-#перемещение 
-#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2 F:\git\my_repo\java\mini_utils\move\tests\f1\
-#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f1\f2 F:\git\my_repo\java\mini_utils\move\tests\f2
-#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f1\f2 F:\git\my_repo\java\mini_utils\move\tests\f2
-#..\move\out\artifacts\move_jar\move.jar ..\tests\f2 ..\tests\f1\f2
-#..\move\out\artifacts\move_jar\move.jar ..\tests\f1\f2 ..\tests\f2
+#перемещение
+echo 1 
+..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f2 F:\git\my_repo\java\mini_utils\move\tests\f1\
+sleep 10;
+echo 2;
+..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f1\f2 F:\git\my_repo\java\mini_utils\move\tests\f2
+sleep 10;
+echo 3;
+..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\f1\f2 F:\git\my_repo\java\mini_utils\move\tests\f2
+sleep 10;
+echo 4;
+..\move\out\artifacts\move_jar\move.jar ..\tests\f2 ..\tests\f1\f2
+sleep 10;
+echo 5;
+..\move\out\artifacts\move_jar\move.jar ..\tests\f1\f2 ..\tests\f2
 
+echo "start test33";
 #одиночные файлы
 #работают с ошибками.
 #после перемещения вв начале имени добавляется знак ; Востальном перемещения работают.
 #test1_result.txt -> ;test1_result.txt
 
-
+sleep 10;
 #;test1_result.txt ->
-##..\move\out\artifacts\move_jar\move.jar "..\tests\;test1_result.txt" "F:\git\my_repo\java\mini_utils\move\tests\"
+echo 1;
+..\move\out\artifacts\move_jar\move.jar "..\tests\;test1_result.txt" "F:\git\my_repo\java\mini_utils\move\tests\"
 ## -> ;;test1_result.txt
 
 #
-#..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\test1_result.txt ..\tests\f2\
-#..\move\out\artifacts\move_jar\move.jar "..\tests\f2\;;;;test1_result.txt" "F:\git\my_repo\java\mini_utils\move\tests\"
+sleep 10;
+..\move\out\artifacts\move_jar\move.jar F:\git\my_repo\java\mini_utils\move\tests\test1_result.txt ..\tests\f2\
+sleep 10;
+..\move\out\artifacts\move_jar\move.jar "..\tests\f2\;;;;test1_result.txt" "F:\git\my_repo\java\mini_utils\move\tests\"
 
 
 
 #группы файлов. Имя задаётся регуляркой.
 # работает
-#..\move\out\artifacts\move_jar\move.jar "F:\git\my_repo\java\mini_utils\move\tests\^.*.txt" F:\git\my_repo\java\mini_utils\move\tests\
+sleep 10;
+..\move\out\artifacts\move_jar\move.jar "F:\git\my_repo\java\mini_utils\move\tests\^.*.txt" F:\git\my_repo\java\mini_utils\move\tests\
+sleep 10;
 ..\move\out\artifacts\move_jar\move.jar "F:\git\my_repo\java\mini_utils\move\tests\f1\^.*.txt" F:\git\my_repo\java\mini_utils\move\tests\
 #sleep 30
-#..\move\out\artifacts\move_jar\move.jar "F:\git\my_repo\java\mini_utils\move\tests\^.*.txt" F:\git\my_repo\java\mini_utils\move\tests\f1\
+sleep 10;
+..\move\out\artifacts\move_jar\move.jar "F:\git\my_repo\java\mini_utils\move\tests\^.*.txt" F:\git\my_repo\java\mini_utils\move\tests\f1\
