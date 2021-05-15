9490d8be344cf497dd040cb84bbf41b9b353308b
nif0 <nif0@yandex.ru>
2021-05-15 19:19:22 +0500

    new file:   java/mini_utils/ls/.idea/artifacts/ls_jar.xml 	new file:   java/mini_utils/ls/out/artifacts/ls_jar/ls.jar 	new file:   java/mini_utils/ls/out/production/ls/META-INF/MANIFEST.MF 	new file:   java/mini_utils/ls/src/META-INF/MANIFEST.MF 	new file:   java/mini_utils/ls/tests/test.ps1 	new file:   java/mini_utils/ls/tests/test_10_result.txt 	new file:   java/mini_utils/ls/tests/test_11_result.txt 	new file:   java/mini_utils/ls/tests/test_1_result.txt 	new file:   java/mini_utils/ls/tests/test_2_result.txt 	new file:   java/mini_utils/ls/tests/test_3_result.txt 	new file:   java/mini_utils/ls/tests/test_4_result.txt 	new file:   java/mini_utils/ls/tests/test_5_result.txt 	new file:   java/mini_utils/ls/tests/test_6_result.txt 	new file:   java/mini_utils/ls/tests/test_7_result.txt 	new file:   java/mini_utils/ls/tests/test_8_result.txt 	new file:   java/mini_utils/ls/tests/test_9_result.txt 	new file:   java/mini_utils/ls/tests/test_ls.ps1

---
+++ java/mini_utils/ls/tests/test.ps1
@@ -0,0 +1,35 @@
+﻿#тестирование ls
+echo "test 1" #для работы необходимо наличие в конце строки косой черты \
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar F:\projects\java\mini_utils\ls\ | Out-File F:\projects\java\mini_utils\ls\tests\test_1_result.txt
+echo "test 2" #
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_2_result.txt
+#тестирую ключи
+#  -D  включить вывод отладочной информации при работе утилиты.
+#  -R  Включить рекурсивную выдачу списка каталогов.(по умолчанию стоит максимальная глубина рекурсии)
+#  -d  Выдавать имена каталогов, как будто они обычные файлы, вместо того, чтобы показывать их содержимое.
+#  -C Напечатать список файлов в колонке с вертикальной сортировкой. Если есть ключ --sort=field, то сортировка идёт по полю field. Иначе, сортировка идёт по первому столбцу с именем файла.
+#  -t  Сортировать по показываемому временному штампу(mtime).
+#  -l, --format=long, --format=verbose В дополнении к имени каждого файла, выводятся тип файла, права доступа к файлу, количество ссылок на файл,
+#    имя владельца, имя группы, размер файла в байтах и временной штамп (время последней модификации файла, если не задано другое).
+#  -a  выводит скрытые файлы
+#  -h печать справки и завершение работы
+#  -H, --human-readable  Добавлять к каждому размеру файла букву размера, например, M для двоичных мегабайт (`мебибайт')
+
+echo "test 3. -D" #не работает
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -D F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_3_result.txt
+echo "test 4. -R" #не работает
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -R F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_4_result.txt
+echo "test 5. -d" #ok
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -d F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_5_result.txt
+echo "test 6. -C" #не работает
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -C F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_6_result.txt
+echo "test 7. -t" #ok
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -t F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_7_result.txt
+echo "test 8. -l" #ok
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -l F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_8_result.txt
+echo "test 9. -a" #ok
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -a F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_9_result.txt
+echo "test 10. -h" #ok
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -h F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_10_result.txt
+echo "test 11. -H" #не работает
+java -jar F:\projects\java\mini_utils\ls\out\artifacts\ls_jar\ls.jar -H F:\projects\java\mini_utils\ls | Out-File F:\projects\java\mini_utils\ls\tests\test_11_result.txt
