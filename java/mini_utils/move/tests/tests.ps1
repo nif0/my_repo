7d6e09d96ec55ef8f41700888920e026bcb17851
nif0 <nif0@yandex.ru>
2021-06-04 12:34:30 +0500

    добавил тестов

--- java/mini_utils/move/tests/tests.ps1
+++ java/mini_utils/move/tests/tests.ps1
@@ -3,7 +3,7 @@
 #стартовая локация: F:\git\my_repo\java\mini_utils\move\tests
 
 #переименование каталога. несуществующий каталог
-echo "start test1 renaming a folder";
+echo "start test1 renaming";
 java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test1\f2 ..\tests\test1\f2_ | Out-File ..\tests\test1\test_1-1_result.txt;
 echo "sleep 10"; sleep 10;
 java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test1\f2_ ..\tests\test1\f2 | Out-File ..\tests\test1\test_1-2_result.txt;
@@ -11,6 +11,20 @@ echo "sleep 10"; sleep 10;
 java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test1\f1 ..\tests\test1\f1_rename | Out-File ..\tests\test1\test_1-3_result.txt;
 echo "sleep 10"; sleep 10;
 java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test1\f1_rename ..\tests\f1 | Out-File ..\tests\test1\test_1-3_result.txt;
+echo "sleep 10"; sleep 10;
+
+echo "start test1 renaming a folder to an existing name";
+#ожидаю сообщение о том, что объект с таким именем уже  существует. Не работает.
+echo "test 1-4. renaming aт object to an existing name" | Out-File ..\tests\test1\text1-4.txt; 
+echo "" | Out-File ..\tests\test1\text1.txt; 
+java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test1\text1.txt ..\tests\test1\text1.txt | Out-File ..\tests\test1\test_1-4_result.txt;
+
+echo "start test 1-5. renaming aт object to non-valid name"; 
+#ожидаю сообщение, вида: "имя файла не является корректным в используемой ФС". Не работает.
+echo "test 1-5. renaming aт object to non-valid name" | Out-File ..\tests\test1\text15.txt; 
+java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test1\text15.txt ..\tests\test1\text1?:/.txt | Out-File ..\tests\test1\test_1-5_result.txt;
+
+
 
 
 #перемещение каталога
@@ -37,6 +51,18 @@ echo "sleep 10"; sleep 10;
 java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test3\f2\test1_result.txt ..\tests\test3\f1\ | Out-File ..\tests\test3\test_3-2_result.txt
 echo "sleep 10"; sleep 10;
 java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test3\f1\test1_result.txt ..\tests\test3\f1\ | Out-File ..\tests\test3\test_3-3_result.txt
+echo "sleep 10"; sleep 10;
+
+echo "start test3 chaotics argument";
+echo "sleep 10"; sleep 10;
+java -jar ..\move\out\artifacts\move_jar\move.jar /sdfsdf/www /dev/null | Out-File ..\tests\test3\test_3-5_result.txt
+echo "sleep 10"; sleep 10;
+
+echo "start test3 move to non-existing folder "
+#ожидаю сообщение о невозможности перемещения в несуществующее место назначения. не работает.
+echo "sleep 10"; sleep 10;
+echo "move to non-existing folder " | Out-File ..\tests\test3\file.fi
+java -jar ..\move\out\artifacts\move_jar\move.jar ..\tests\test3\file.fi ..\tests\test3\qqqwww | Out-File ..\tests\test3\test_3-6_result.txt
 echo "sleep 10"; sleep 10;
 
 
@@ -91,3 +117,4 @@ java -jar ..\move\out\artifacts\move_jar\move.jar -b ..\tests\test5\f1\^.*.txt .
 echo "sleep 10"; sleep 10;
 echo "key: -h. sleep 2"; sleep 2;
 java -jar ..\move\out\artifacts\move_jar\move.jar -h ..\tests\test5\f2\^.*.txt ..\tests\test5\f1\ | Out-File ..\tests\test5\test_5-7_result.txt
+
